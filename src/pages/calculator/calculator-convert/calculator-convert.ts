6a33f789494c455f4dac9d0a1bcf70331c10c652
lamiqv <laumiqv@gmail.com>
2020-06-01 15:14:23 +0300

    calculator page init

---
+++ src/pages/calculator/calculator-convert/calculator-convert.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-cards',
+  templateUrl: 'calculator-convert.html',
+  styleUrls: ['../calculator.scss']
+})
+export class CalculatorConvertPage {
+  constructor() { }
+}
---
+++ src/pages/calculator/calculator-send/calculator-send.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-cards',
+  templateUrl: 'calculator-send.html',
+  styleUrls: ['../calculator.scss']
+})
+export class CalculatorSendPage {
+  constructor() { }
+}
