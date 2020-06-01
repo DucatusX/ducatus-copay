10297964da97a9094bb1a9ba85727f23cbd3aeb3
lamiqv <laumiqv@gmail.com>
2020-06-01 21:56:49 +0300

    convert change duc-ducx/eth/btc, ducx-duc

--- src/pages/calculator/calculator-parameters.ts
+++ src/pages/calculator/calculator-parameters.ts
@@ -1,7 +1,9 @@
 export const convertCoins = {
-  DUC: { name: 'DUC', items: ['DUCX', 'ETH', 'BTC'] },
-  DUCX: { name: 'DUCX', items: ['DUC'] }
+  'DUC': { name: 'DUC', items: ['DUCX', 'ETH', 'BTC'] },
+  'DUCX': { name: 'DUCX', items: ['DUC'] }
 }
+
+export const convertGetCoins = ['DUC', 'DUCX'];
 
 export const coinInfo = {
   'DUC': 'Ducatus',
