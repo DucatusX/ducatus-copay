3ab42a081ffc86c0d38b19702456ad9e7f675695
lamiqv <laumiqv@gmail.com>
2020-06-02 03:07:09 +0300

    add icons, set rates, exhcange functions

--- src/pages/calculator/calculator-parameters.ts
+++ src/pages/calculator/calculator-parameters.ts
@@ -1,13 +1,13 @@
 export const convertCoins = {
-  'DUC': { name: 'DUC', items: ['DUCX', 'ETH', 'BTC'] },
-  'DUCX': { name: 'DUCX', items: ['DUC'] }
+  DUC: { name: 'DUC', items: ['DUCX', 'ETH', 'BTC'] },
+  DUCX: { name: 'DUCX', items: ['DUC'] }
 }
 
 export const convertGetCoins = ['DUC', 'DUCX'];
 
 export const coinInfo = {
-  'DUC': 'Ducatus',
-  'DUCX': 'DucatusX',
-  'ETH': 'Etherium',
-  'BTC': 'Bitcoin'
+  DUC: 'Ducatus',
+  DUCX: 'DucatusX',
+  ETH: 'Etherium',
+  BTC: 'Bitcoin'
 }
