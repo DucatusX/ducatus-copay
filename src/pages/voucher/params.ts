3a8b3bc38d0a220d954ee5cf96e2eb416e55a5bc
Amary <laumiqv@gmail.com>
2020-08-05 16:09:19 +0300

    change req url: http > https for voucher & deposit

--- src/pages/voucher/params.ts
+++ src/pages/voucher/params.ts
@@ -1,2 +1,2 @@
-export const VOUCHER_URL_REQUEST = 'http://ducsite.rocknblock.io/api/v3/';
+&export const VOUCHER_URL_REQUEST = 'https://ducsite.rocknblock.io/api/v3/';
 // export const VOUCHER_URL_REQUEST = 'https://www.ducatuscoins.com/api/v3/';
