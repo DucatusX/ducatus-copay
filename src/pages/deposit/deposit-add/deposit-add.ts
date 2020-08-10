53a708d7262a1457bebb89f3ee4764b04c647148
Amary <laumiqv@gmail.com>
2020-08-10 23:11:50 +0300

    fix problems, change deposit & voucher to mainnet

--- src/pages/deposit/deposit-add/deposit-add.ts
+++ src/pages/deposit/deposit-add/deposit-add.ts
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
