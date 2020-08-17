6acacd74f747efcf7da747fd557a5c812c7bb8e5
Amary <laumiqv@gmail.com>
2020-08-17 18:44:45 +0300

    upd api link

--- src/pages/freeze/params.ts
+++ src/pages/freeze/params.ts
@@ -1,2 +1,2 @@
-// export const FREEZE_URL_REQUEST = 'https://www.ducatuscoins.com/api/v1/';
-export const FREEZE_URL_REQUEST = 'https://ducsite.rocknblock.io/api/v1/';
+// export const FREEZE_URL_REQUEST = 'https://www.ducatuscoins.com/api/v3/';
+export const FREEZE_URL_REQUEST = 'https://ducsite.rocknblock.io/api/v3/';
