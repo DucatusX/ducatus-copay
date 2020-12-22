945d86ba7f9cc6b638125bfcb017ecab9df09580
ottebrut <ott.andrey.ao@gmail.com>
2020-12-22 17:54:22 +0300

    typo

--- src/pages/calculator/calculator-convert/calculator-convert.ts
+++ src/pages/calculator/calculator-convert/calculator-convert.ts
@@ -249,7 +249,7 @@ export class CalculatorConvertPage {
         const dailyAvailable = res['daily_available'] / DECIMALS;
         const weeklyAvailable = res['weekly_available'] / DECIMALS;
         if (dailyAvailable < amountSend) {
-          throw new Error('Weekly DUCX swap limit is 1000 DUC. This day you can swap no more than ' + dailyAvailable + ' DUC.');
+          throw new Error('Daily DUCX swap limit is 1000 DUC. This day you can swap no more than ' + dailyAvailable + ' DUC.');
         } else if (weeklyAvailable < amountSend) {
           throw new Error('Weekly DUCX swap limit is 5000 DUC. This week you can swap no more than ' + weeklyAvailable + ' DUC.');
         } else {
