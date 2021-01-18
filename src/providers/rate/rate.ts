c7b931d27b6da36738b545ad43cc4fe616b1a031
Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
2021-01-18 13:36:34 +0300

    prod bitcore link

--- src/providers/rate/rate.ts
+++ src/providers/rate/rate.ts
@@ -12,8 +12,7 @@ export class RateProvider {
   private ratesAvailable = {} as CoinsMap<boolean>;
   private rateServiceUrl = {} as CoinsMap<string>;
 
-  private fiatRateAPIUrl =
-    'https://duc-ws-dev.rocknblock.io/bws/api/v1/fiatrates';
+  private fiatRateAPIUrl = 'https://ducws.rocknblock.io/bws/api/v1/fiatrates';
 
   constructor(
     private currencyProvider: CurrencyProvider,
