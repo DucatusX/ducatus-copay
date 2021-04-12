d1153f471b1917ef1bf702a9068a12a8a80bc675
Amary Filo <46869459+laumiqv@users.noreply.github.com>
2021-04-12 18:52:17 +0300

    Release/2.6.3 (#18)
    * new version
    
    * v2.5.9
    
    * fix create tokens
    
    * change version
    
    * livechat
    
    * legacy qr code
    
    * release/2.6.0 (#8)
    
    * Feature/duc 390 (#7)
    
    * hardcode to duc and ducx api
    
    * dev bitcore
    
    * update wallets rate
    
    * update all rates
    
    * dev bitcore to prod
    
    * delete debugger
    
    * release/2.6.0
    
    * Release/2.6.1 (#12)
    
    * Feature/duc 390 (#7)
    
    * hardcode to duc and ducx api
    
    * dev bitcore
    
    * update wallets rate
    
    * update all rates
    
    * dev bitcore to prod
    
    * delete debugger
    
    * release/2.6.0 (#9)
    
    * Feature/duc 417, duc 415 (#10)
    
    * filter wallets in vaucher page
    
    * api provider, bitcore api
    
    * more api for api provider
    
    * only eth wallets for usdc
    
    * filter eth wallets
    
    * release v2.6.1
    
    Co-authored-by: Amary Filo <46869459+laumiqv@users.noreply.github.com>
    
    * Release/2.6.2 (#15)
    
    * Feature/duc 390 (#7)
    
    * hardcode to duc and ducx api
    
    * dev bitcore
    
    * update wallets rate
    
    * update all rates
    
    * dev bitcore to prod
    
    * delete debugger
    
    * release/2.6.0 (#9)
    
    * Feature/duc 417, duc 415 (#10)
    
    * filter wallets in vaucher page
    
    * api provider, bitcore api
    
    * more api for api provider
    
    * only eth wallets for usdc
    
    * filter eth wallets
    
    * release v2.6.1 (#13)
    
    * new version
    
    * v2.5.9
    
    * fix create tokens
    
    * change version
    
    * livechat
    
    * legacy qr code
    
    * release/2.6.0 (#8)
    
    * Feature/duc 390 (#7)
    
    * hardcode to duc and ducx api
    
    * dev bitcore
    
    * update wallets rate
    
    * update all rates
    
    * dev bitcore to prod
    
    * delete debugger
    
    * release/2.6.0
    
    * release v2.6.1
    
    Co-authored-by: Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
    Co-authored-by: Amary Filo <46869459+laumiqv@users.noreply.github.com>
    
    * change rate api (#14)
    
    * release v2.6.2
    
    Co-authored-by: Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
    Co-authored-by: Amary Filo <46869459+laumiqv@users.noreply.github.com>
    
    * release fixes
    
    Co-authored-by: Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
    Co-authored-by: CleanBread <37740558+CleanBread@users.noreply.github.com>
    Co-authored-by: Gleb Chistyakov <gleb4ist@gmail.com>

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,17 +1,19 @@
 export class ApiProvider {
-  isProduction = true;
+  public isProduction = true;
   private config = {
     prod: {
       bitcore: 'https://ducws.rocknblock.io',
       ducatuscoins: 'https://www.ducatuscoins.com',
       crowdsale: 'https://tokenization.centuriongm.com',
-      pog: 'https://d-pog.com'
+      pog: 'https://d-pog.com',
+      ratesApi: 'https://rates.ducatuscoins.com/api/v1/rates/'
     },
     develop: {
       bitcore: 'https://duc-ws-dev.rocknblock.io',
       ducatuscoins: 'https://ducsite.rocknblock.io',
       crowdsale: 'http://duccrowdsale.rocknblock.io',
-      pog: 'https://devgold.rocknblock.io'
+      pog: 'https://devgold.rocknblock.io',
+      ratesApi: 'https://ducexpl.rocknblock.io/api/v1/rates/'
     }
   };
 
