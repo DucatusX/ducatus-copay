2b059609c60a9fd8a7890003a31d0d20212f8ade
seredganyha <karix05@yandex.ru>
2022-01-18 11:10:07 +0300

    fix

--- src/pages/calculator/calculator-parameters.ts
+++ src/pages/calculator/calculator-parameters.ts
@@ -1,11 +1,10 @@
 export const convertCoins = {
   DUC: { name: 'DUC', items: ['DUCX'] },
-  DUCX: { name: 'DUCX', items: ['DUC']},
+  DUCX: { name: 'DUCX', items: ['DUC', 'WDUCX'] },
   BTC: { name: 'BTC', items: ['DUC']},
   ETH: { name: 'ETH', items: ['DUC']},
-  // WDUCX: { name: 'WDUCX', items: ['DUCX'] },
+  WDUCX: { name: 'WDUCX', items: ['DUCX'] }
 };
-
 
 export const convertSendCoins = ['DUC','DUCX', 'ETH' , 'BTC',]
 export const convertGetCoins =  ['DUCX'];
