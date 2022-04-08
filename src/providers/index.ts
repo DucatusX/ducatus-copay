d8cdaed80cc9ad536644b7e2c5c8f56b3b1aff7b
seredganyha <karix05@yandex.ru>
2022-04-08 11:11:22 +0300

    add new provider.
    provider that processes and formats data
     05 - > 0.5
      .   ->  0.
     max length = 20
    decimals for coins

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -25,6 +25,7 @@ export { AnalyticsProvider } from '../providers/analytics/analytics';
 export { AppIdentityProvider } from '../providers/app-identity/app-identity';
 export { AppProvider } from '../providers/app/app';
 export { BackupProvider } from '../providers/backup/backup';
+export { FormControllerProvider } from './form-contoller/form-controller';
 export {
   BitPayAccountProvider
 } from '../providers/bitpay-account/bitpay-account';
