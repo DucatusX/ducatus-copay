9e5800fb72a23483ccce52dbaca5af489bcc761d
seredganyha <karix05@yandex.ru>
2022-04-11 15:59:15 +0300

    elimination of comments after the review

--- src/pages/calculator/calculator-parameters.ts
+++ src/pages/calculator/calculator-parameters.ts
@@ -1,12 +1,12 @@
 export const convertCoins = {
   DUC: { name: 'DUC', items: ['DUCX'] },
   DUCX: { name: 'DUCX', items: ['DUC', 'WDUCX'] },
-  BTC: { name: 'BTC', items: ['DUC']},
-  ETH: { name: 'ETH', items: ['DUC']},
+  BTC: { name: 'BTC', items: ['DUC'] },
+  ETH: { name: 'ETH', items: ['DUC'] },
   WDUCX: { name: 'WDUCX', items: ['DUCX'] }
 };
 
-export const convertSendCoins = ['DUC','DUCX', 'ETH' , 'BTC',];
+export const convertSendCoins = ['DUC', 'DUCX', 'ETH' , 'BTC'];
 export const convertGetCoins =  ['DUCX'];
 
 export interface ICoinInfo {
