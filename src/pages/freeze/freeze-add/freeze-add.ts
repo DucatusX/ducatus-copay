9c32ef11f1118e8b81ef14708ab57fa2c029d35e
seredganyha <48453416+seredganyha@users.noreply.github.com>
2022-05-06 10:19:06 +0300

    added the ability to send deposits with a new back (#161)

--- src/pages/freeze/freeze-add/freeze-add.ts
+++ src/pages/freeze/freeze-add/freeze-add.ts
@@ -2,14 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController, NavController } from 'ionic-angular';
+import { BwcProvider } from '../../../providers';
 
 import { ActionSheetProvider } from '../../../providers/action-sheet/action-sheet';
 import { ApiProvider } from '../../../providers/api/api';
 import { IncomingDataProvider } from '../../../providers/incoming-data/incoming-data';
+import { Logger } from '../../../providers/logger/logger';
 import { ProfileProvider } from '../../../providers/profile/profile';
 import { TxFormatProvider } from '../../../providers/tx-format/tx-format';
 import { WalletProvider } from '../../../providers/wallet/wallet';
-
 import { BackupKeyPage } from '../../backup/backup-key/backup-key';
 
 @Component({
@@ -17,14 +18,19 @@ import { BackupKeyPage } from '../../backup/backup-key/backup-key';
   templateUrl: 'freeze-add.html'
 })
 export class FreezeAddPage {
-  public FreezeGroup: FormGroup;
-
+  public DepositGroup: FormGroup;
   public wallet: any;
   public walletAddresses: any;
-  public freezeLoading = false;
-  public amountWallet = null;
-  public freezeDays = null;
+  public sendLength: number = 0;
+  public depositLoading = false;
+  public amountWithPercent: any;
+  public amountWallet = 0;
+  public depositMonth = 0;
+  public depositPercent: number;
   public maxAmount = 0;
+  public useSendMax: boolean = false;
+  public invalidAddress: boolean = false;
+  public ducatuscore: any;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -36,240 +42,235 @@ export class FreezeAddPage {
     private alertCtrl: AlertController,
     private navCtrl: NavController,
     private httpClient: HttpClient,
-    private apiProvider: ApiProvider
+    private apiProvider: ApiProvider,
+    private logger: Logger,
+    private bwcProvider: BwcProvider
   ) {
-    this.FreezeGroup = this.formBuilder.group({
-      Address: [
+
+    this.ducatuscore = this.bwcProvider.getDucatuscore();
+    
+    this.DepositGroup = this.formBuilder.group({
+      AddressFrom: [
         '',
         Validators.compose([Validators.minLength(34), Validators.required])
       ],
       AddressTo: [
         '',
-        Validators.compose([
-          Validators.minLength(34),
-          Validators.maxLength(34),
-          Validators.required
-        ])
+        Validators.compose([Validators.minLength(34), Validators.required])
       ],
       Amount: [
         '',
         Validators.compose([
-          Validators.minLength(1),
-          Validators.required,
-          Validators.min(0)
+        Validators.minLength(1),
+        Validators.required,
+        Validators.min(0)
         ])
       ],
-      Days: [
-        '',
-        Validators.compose([
-          Validators.minLength(1),
-          Validators.required,
-          Validators.min(0)
-        ])
-      ]
+      Month: [''],
     });
   }
 
-  ionViewWillEnter() {
+  public async ionViewWillEnter(): Promise<void> {
+    this.viewAlertAttention('confirmation of the deposit can take up to 1 business day.');
+
     const wallets = this.profileProvider.getWallets({
       showHidden: true,
       backedUp: true
     });
+    const coinWallets: any = await this.walletProvider.getWalletsByCoin(wallets, 'duc');
 
-    this.walletProvider.getWalletsByCoin(wallets, 'duc').then(res => {
-      const result: any = res;
+    if ( coinWallets.count <= 0 ) {
+      this.showModal('needbackup');
+    } 
+    
+    this.walletAddresses = coinWallets.wallets;
+  }
 
-      if (result.count <= 0) this.showModal('needbackup');
-
-      this.walletAddresses = result.wallets;
+  private viewAlertAttention(message: string): void {
+    const alert = this.alertCtrl.create({
+      cssClass: 'voucher-alert',
+      title: '<img src ="./assets/img/icon-attantion.svg" width="42px" height="42px">',
+      message,
+      buttons: [{ text: 'Ok' }]
     });
+
+    alert.present();
   }
 
-  public changeAmountToMax() {
-    this.FreezeGroup.controls.Amount.setValue(this.maxAmount);
+  public changeAmountToMax(): void {
+    this.DepositGroup.controls.Amount.setValue(this.maxAmount);
+    this.changeAmount();
   }
 
-  public openAddressList() {
-    if (!this.freezeLoading) {
+  public changeAddressTo(addressTo) {
+    const isValid = this.ducatuscore.Address.isValid(addressTo, 'livenet');
+    if (addressTo === '') {
+      this.invalidAddress = false;
+    }
+    else if (!isValid) {
+      this.invalidAddress = true;
+    }      
+  }
+
+  public changeAmount(): void {
+    const floatAmount = parseFloat(this.DepositGroup.value.Amount);
+    
+    if ( floatAmount < 0 ) {
+      this.DepositGroup.controls.Amount.setValue('0');
+    }
+
+    let amount = floatAmount || 0;
+    
+    if ( floatAmount <= 0 ) {
+      amount = 0;
+    }
+
+    const amountWithPercentValue = (
+      amount
+      * (parseFloat(this.DepositGroup.value.Percent) / 100)
+      * (parseFloat(this.DepositGroup.value.Month) / 12)
+    ).toFixed(4);
+
+    this.amountWithPercent = amountWithPercentValue;
+  }
+
+  public openAddressList(): void {
+
+    if ( !this.depositLoading ) {
       const infoSheet = this.actionSheetProvider.createInfoSheet(
         'convertor-address',
         { wallet: this.walletAddresses }
       );
       infoSheet.present();
+
       infoSheet.onDidDismiss(option => {
         if (option) {
-          this.FreezeGroup.value.Address = option;
-
-          this.wallet = this.walletAddresses.find(t => t.address === option);
+          this.DepositGroup.value.AddressFrom = option;
+          this.useSendMax = false;
+          this.wallet = this.walletAddresses.find(wallet => wallet.address === option);
           this.sendMax();
 
-          if (this.wallet.needsBackup)
+          if ( this.wallet.needsBackup ) {
             this.navCtrl.push(BackupKeyPage, {
               keyId: this.wallet.keyId
             });
+          }
         }
       });
     }
   }
 
-  public amountChange(_event) {
-    this.amountWallet = Number(
-      this.FreezeGroup.controls['Amount'].value
-        .toString()
-        .replace(/[^0-9.]/g, '')
-        .replace(/(\..*)\./g, '$1')
-    );
-    this.FreezeGroup.controls['Amount'].setValue(
-      Number(
-        this.FreezeGroup.controls['Amount'].value
-          .toString()
-          .replace(/[^0-9.]/g, '')
-          .replace(/(\..*)\./g, '$1')
-      )
-    );
-  }
+  public async sendMax(): Promise<void> {
+    const { token } = this.wallet.wallet.credentials;
+    const tokenAddress = token && token.address || '';
+    const amount = await this.walletProvider.getBalance(this.wallet.wallet, { tokenAddress });
 
-  public daysChange(_event) {
-    this.freezeDays = Number(
-      this.FreezeGroup.controls['Days'].value.toString().replace(/(\.\d+)+/, '')
+    this.maxAmount = amount.availableAmount / 100000000;
+    this.useSendMax = true;
+  }
+  public monthChange(_event) {
+    this.depositMonth = Number(
+      this.DepositGroup.controls['Month'].value.toString().replace(/(\.\d+)+/, '')
     );
-    this.FreezeGroup.controls['Days'].setValue(
+    this.DepositGroup.controls['Month'].setValue(
       Number(
-        this.FreezeGroup.controls['Days'].value
+        this.DepositGroup.controls['Month'].value
           .toString()
           .replace(/(\.\d+)+/, '')
       )
     );
   }
 
-  public sendMax() {
-    const { token } = this.wallet.wallet.credentials;
-
-    this.walletProvider
-      .getBalance(this.wallet.wallet, {
-        tokenAddress: token ? token.address : ''
-      })
-      .then(resp => {
-        this.maxAmount = resp.availableAmount / 100000000;
-      });
-  }
-
-  private generateFreeze(
-    wallet_id: string,
+  private async generateDeposit(
+    wallet: string,
     duc_address: string,
-    receiver_user_public_key: string,
-    sender_user_public_key: string,
-    private_path: number,
-    lock_days: number
-  ) {
+    lock_months: string,
+  ): Promise<any> {
+    const address = this.apiProvider.getAddresses().deposit + 'user/deposits/create-without-dividends/';
     return this.httpClient
-      .post(`${this.apiProvider.getAddresses().ducatuscoins}/api/v3/generate_deposit_without_dividends/`, {
-        wallet_id,
+      .post(address, {
+        wallet,
         duc_address,
-        receiver_user_public_key,
-        sender_user_public_key,
-        lock_days,
-        private_path
+        lock_months,
       })
       .toPromise();
   }
 
-  public async generateUserFreeze() {
-    this.freezeLoading = true;
+  public async generateUserDeposit(): Promise<void> {
+    this.depositLoading = true;
+    
+    const resultPrepare: any = await this.walletProvider.prepareAdd(this.walletAddresses, this.DepositGroup.value.AddressFrom);
+    
+    try {
+      const deposit: any = await this.generateDeposit(
+        resultPrepare.wallet.walletId,
+        this.DepositGroup.value.AddressTo,
+        String(this.DepositGroup.value.Month)
+      );
+      
+      if (deposit.ducAddress) {
+        const addressView = this.walletProvider.getAddressView(
+          resultPrepare.wallet.wallet.coin,
+          resultPrepare.wallet.wallet.network,
+          deposit.ducAddress,
+          true
+        );
 
-    const addresses = this.walletAddresses;
-    const addressFrom = this.FreezeGroup.value.Address;
-    const addressTo = this.FreezeGroup.value.AddressTo;
-    const days = this.FreezeGroup.value.Days;
+        const parsedAmount = this.txFormatProvider.parseAmount(
+          resultPrepare.wallet.wallet.coin.toLowerCase(),
+          this.DepositGroup.value.Amount,
+          resultPrepare.wallet.wallet.coin.toUpperCase()
+        );
 
-    const receiverAddress = await this.walletProvider
-      .getInfoByAddress(addresses, addressFrom, addressTo)
-      .then(res => {
-        return res;
-      });
+        this.useSendMax = false;  
 
-    const receiverData = (await this.walletProvider
-      .prepareAddFreeze(receiverAddress.wallet, receiverAddress.address[0])
-      .then(res => {
-        return res;
-      })) as any;
-
-    const senderData = (await this.walletProvider
-      .prepareAdd(addresses, addressFrom)
-      .then(res => {
-        return res;
-      })) as any;
-
-    this.generateFreeze(
-      receiverData.walletId,
-      addressTo,
-      receiverData.pubKey,
-      senderData.pubKey,
-      receiverData.path,
-      Number(days)
-    )
-      .then(res => {
-        const result: any = res;
-
-        if (result.cltv_details.locked_duc_address) {
-          const addressView = this.walletProvider.getAddressView(
-            senderData.wallet.wallet.coin,
-            senderData.wallet.wallet.network,
-            result.cltv_details.locked_duc_address,
-            true
-          );
-
-          const parsedAmount = this.txFormatProvider.parseAmount(
-            senderData.wallet.wallet.coin.toLowerCase(),
-            this.FreezeGroup.value.Amount,
-            senderData.wallet.wallet.coin.toUpperCase()
-          );
-
-          const redirParms = {
-            activePage: 'ScanPage',
-            walletId: senderData.wallet.wallet.id,
-            amount: parsedAmount.amountSat
-          };
-
-          this.incomingDataProvider.redir(addressView, redirParms);
+        if (this.DepositGroup.value.Amount === this.maxAmount) {
+          this.useSendMax = true;
         }
-      })
-      .catch(() => this.showModal('network'));
+
+        const redirParms = {
+          activePage: 'ScanPage',
+          walletId: resultPrepare.wallet.wallet.id,
+          amount: parsedAmount.amountSat,
+          useSendMax: this.useSendMax
+        };
+
+        this.incomingDataProvider.redir(addressView, redirParms);
+      }
+    } catch(err) {
+      this.showModal('network');
+      this.logger.debug(err);
+    }
   }
 
-  private showModal(type: string) {
+  private showModal(type: string): void {
     const modalAnswers = {
       network: {
-        title:
-          '<img src ="./assets/img/icon-attantion.svg" width="42px" height="42px">',
+        title: '<img src ="./assets/img/icon-attantion.svg" width="42px" height="42px">',
         text: 'Something went wrong, try again',
         button: 'OK',
         handler: () => {
-          this.freezeLoading = false;
-          if (type != 'network') {
-            this.FreezeGroup.value.Address = '';
-            this.FreezeGroup.value.Amount = '';
+          this.depositLoading = false;
+
+          if ( type != 'network' ) {
+            this.DepositGroup.value.AddressFrom = '';
+            this.DepositGroup.value.Amount = '';
           }
         },
         enableBackdropDismiss: false
       },
       needbackup: {
-        title:
-          '<img src ="./assets/img/icon-attantion.svg" width="42px" height="42px">',
+        title: '<img src ="./assets/img/icon-attantion.svg" width="42px" height="42px">',
         text: 'Needs Backup',
         button: 'OK',
-        handler: () => {
-          this.navCtrl.pop();
-        },
+        handler: () => { this.navCtrl.pop(); },
         enableBackdropDismiss: false
       }
     };
 
-    const answers = modalAnswers[type]
-      ? modalAnswers[type]
-      : modalAnswers['error'];
+    const answers = modalAnswers[type] || modalAnswers['error'];
 
-    let alert = this.alertCtrl.create({
+    const alert = this.alertCtrl.create({
       cssClass: 'voucher-alert',
       title: answers.title,
       message: answers.text,
